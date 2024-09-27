4e48daf1d763ae29e379139c254e6e372a0dedbf
Tristan Mangin <tw.mangin@gmail.com>
2024-09-27 16:06:24 -0400

    RCOS Meeting 9/24

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -7,74 +7,45 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  // ---------------------------------------------------------------------------
-  // F U N D A M E N T A L S
-  /*
-  VARIABLES: Store info into variables
- 
-  String name = "Tristan Mangin";
-  int age = 21;
-  double pi = 3.14;
-  bool isBeginner = true;
- 
-  MATH OPERATORS 
-  
-  *Same as Java*
-
-  LOGICAL OPERATORS
-
-  *Same as Java*
-  */
-
-  // ---------------------------------------------------------------------------
-
-  // C O N T R O L  F L O W
-  /*
-  if (condition) {
-    do something
-  }else {
-    do something else
-  }
-
-  switch (expression) {
-    case value:
-
-      break;
-    case value:
-
-      break:
-    default:
-  }
-
-  for (int i = 0; i < range; i++) {
-    do something for each i in range
-  }
-
-  while (condition) {
-    do something while condition is true
-    progress until condition is false
-  }
-
-  break -> breaks out of loop
-  continue -> move on to next interation
-
-  */
-
-  // ---------------------------------------------------------------------------
-
-  // F U N C T I O N S
-
-  /*
-  
-  */
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text("App Bar"),
+            backgroundColor: Colors.green,
+            elevation: 0,
+            leading: Icon(Icons.menu),
+            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+          ),
+          // body: Center(
+          //   child: Container(
+          //       height: 300,
+          //       width: 300,
+          //       decoration: BoxDecoration(
+          //           color: Colors.green,
+          //           // Curve the corners
+          //           borderRadius: BorderRadius.circular(20)),
+          //       padding: EdgeInsets.all(25),
+          //       // child: Text(
+          //       //   "Tristan Mangin",
+          //       //   style: TextStyle(
+          //       //     color: Colors.white,
+          //       //     fontSize: 28,
+          //       //     fontWeight: FontWeight.bold,
+          //       //   )
+          //       // ),
+          //       child: Icon(
+          //         Icons.favorite,
+          //         color: Colors.white,
+          //         size: 64,
+          //       )),
+          // )
+				
+				),
     );
   }
 }
